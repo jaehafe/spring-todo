bd2c31de05bb49e9e867763a4f6e9d34aad9f6e4
Jaeha Lee <wogkdkrm112@gmail.com>
2024-01-28 19:11:23 +0900

    feat: mysql, TodoRepository

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo (id, description, done, target_date, username)
+values (10001, 'Learn AWS', false, CURRENT_DATE(), 'jaeha');
+
+insert into todo (id, description, done, target_date, username)
+values (10002, 'Learn AWS1', false, CURRENT_DATE(), 'jaeha');
+
+insert into todo (id, description, done, target_date, username)
+values (10003, 'Learn AWS2', true, CURRENT_DATE(), 'jaeha');
